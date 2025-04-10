a8ed7e967267f7d558b6040cdecf48d9d6d6ab75
Vladimir Sanchez <vladypuggu@gmail.com>
2025-04-09 19:36:36 -0600

    Created root-component page

--- src/root-component.js
+++ src/root-component.js
@@ -1,1 +1,52 @@
-import{LitElement,htnl,css,nothing}from'lit'
+import { LitElement, html, css, nothing } from 'lit'
+
+import { NavbarComponent } from './navbar-component'
+
+import { CommonSideEffects } from './common-side-effects-component'
+
+export class RootComponent extends LitElement {
+    static get properties() {
+        currentPage: {
+            type: String
+        }
+    }
+
+    constructor() {
+        super()
+        this.currentPage = "homepage"
+    }
+
+    static get styles() {
+        return css`
+        #root-container{
+        background-color:#3f68df;
+
+        padding:2vw;
+        }
+        `
+    }
+
+    handlePageChange(e) {
+        this.currentPage = e.detail
+    }
+
+    render() {
+        let pageTemplate = nothing
+        switch (this.currentPage) {
+            case 'common-side-effects':
+                pageTemplate = html`<common-side-effects-component></common-side-effects-component>`
+                break;
+        }
+
+        return html`
+        <div id="root-container">
+            <navbar-component @change-page="${this.handlerPageChange}"></navbar-component>
+            <h1>Pagina para mostrar caricaturas de adult swim</h1>
+
+            ${pageTemplate}
+    </div>
+    `
+    }
+}
+
+window.customElements.define('root-component', RootComponent)
